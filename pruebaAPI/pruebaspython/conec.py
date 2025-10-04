4053885fc2d06f1a663d5bbbbcf53b1a97ce3d57
SamuelmamuU <samuelmamu1@outlook.com>
2025-10-04 16:30:43 -0600

    ya funciona la genenracion de mapas con la api

--- pruebaAPI/pruebaspython/conec.py
+++ pruebaAPI/pruebaspython/conec.py
@@ -1,8 +1,35 @@
 import ee
+import geemap
+
 
 # Inicializar
-ee.Initialize()
+
+ee.Authenticate()
+ee.Initialize(project='super-bloom')
 
 # Probar si funciona
-image = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20230701T183559_20230701T183602_T11SLT')
-print(image.getInfo())
+# Definir región y fecha
+region = ee.Geometry.Rectangle([-118.6,34.4,-117.8,35.0])
+start = '2023-04-01'
+end = '2023-04-30'
+
+# Colección Sentinel-2
+collection = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED') \
+    .filterDate(start, end) \
+    .filterBounds(region)
+
+# Mediana de todas las imágenes en el periodo
+image = collection.median()
+
+# Calcular NDVI
+ndvi = image.normalizedDifference(['B8', 'B4']).rename('NDVI')
+
+# Crear mapa con geemap
+Map = geemap.Map(center=[34.7, -118.2], zoom=10)
+
+# Añadir NDVI al mapa
+ndvi_params = {'min': 0, 'max': 1, 'palette': ['brown','yellow','green']}
+Map.addLayer(ndvi, ndvi_params, 'NDVI Abril')
+
+# Mostrar mapa interactivo
+Map.to_html('NDVI_Abril.html')
